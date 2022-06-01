bde0f72bb2ab3e0232ae1869fccbf427f85a5150
Aftab Sidhu <Aftab180@gmail.com>
2022-06-01 19:06:42 -0400

    update

---
+++ src/components/dropDown/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const DropDown = (props) => {
+
+    const {} = props;
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default DropDown;
