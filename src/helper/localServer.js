fa5f7217baeffebf9fb49eb252dedbf301614800
Aftab Sidhu <Aftab180@gmail.com>
2022-06-01 21:10:24 -0400

    update

--- src/helper/localServer.js
+++ src/helper/localServer.js
@@ -1,5 +1,32 @@
+export const getAllFavs = async () => {
+    return await fetch('http://localhost:3003/favs')
+    .then(res => { return res.json()})
+    .then(data => { return data})
+    .catch(err => console.log('Error: ',err))
+}
+
 export const addToFavs = async (data) => {
     await fetch(`http://localhost:3003/favs`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => { return res.json()})
+    .then(data => console.log('Resp: ',data))
+    .catch(err => console.log('Error: ',err))
+}
+
+export const getAllHistory = async () => {
+    return await fetch('http://localhost:3003/history')
+    .then(res => { return res.json()})
+    .then(data => { return data})
+    .catch(err => console.log('Error: ',err))
+}
+
+export const addToHistory = async (data) => {
+    await fetch(`http://localhost:3003/history`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
