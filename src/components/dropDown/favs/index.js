1758675f59a0ad61a486f33929a926798f54c54a
Aftab Sidhu <Aftab180@gmail.com>
2022-06-02 22:49:35 -0400

    update

--- src/components/dropDown/favs/index.js
+++ src/components/dropDown/favs/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllFavs } from '../../../helper/localServer';
 
 const Favs = () => {
+
+    const [favs, setFavs] = useState({});
+
+    const getAllFavorites = async () => {
+        setFavs( await getAllFavs())
+    }
+
+    useEffect(() => {
+        getAllFavorites();
+    },[])
+
     return (
         <div>
-            <p>Favorite</p>
+            {Object.keys(favs).length > 0 ? (
+                <div>
+                    <h4>{favs.name}</h4>
+                    <p>{favs.formatted_address}</p>
+                </div>
+            ) : <p>OOPS! NO FAVORITES</p>} 
         </div>
     )
 }
--- src/components/dropDown/history/index.js
+++ src/components/dropDown/history/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllHistory } from '../../../helper/localServer';
 
 const History = () => {
+
+    const [history, setHistory] = useState({});
+
+    const getHistory = async () => {
+        setHistory( await getAllHistory())
+    }
+
+    useEffect(() => {
+        getHistory();
+    },[])
+
     return (
         <div>
-            <p>History</p>
+            {Object.keys(history).length > 0 ? (
+                <div>
+                    <h4>{history.name}</h4>
+                    <p>{history.formatted_address}</p>
+                </div>
+            ) : <p>OOPS! NO HISTORY</p>} 
         </div>
     )
 }
